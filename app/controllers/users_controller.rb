1d95dc17b688e4b98e63622128cd6e3fe5bd638f
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-04-13 08:06:38 +0000

    [Finish]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     @post_images = @user.post_images
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
 
 private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit] , path_names: { new: 'sign_up' }
+  resources :users, only: [:new, :create, :show, :edit, :update] , path_names: { new: 'sign_up' }
 
   resource :session
   resources :passwords, param: :token
