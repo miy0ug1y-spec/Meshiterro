1fc187c967687006fb28af48cb9ebe76c44908d6
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-04-14 05:00:21 +0000

    [Finish]4-19

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   has_one_attached :profile_image
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create, :destroy] do 
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy] 
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:new, :create, :show, :edit, :update] , path_names: { new: 'sign_up' }
