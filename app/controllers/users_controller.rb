7ffe5f29ec1a27dcd5bfe2ad9cdb5cd4c3d2e29e
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-04-15 02:30:24 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   allow_unauthenticated_access only: [:new, :create]
 
   def new
@@ -24,13 +25,21 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
 private
   def user_params
     params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
   end
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == Current.user.id
+      redirect_to post_images_path
+    end
+  end
 end
+
+
